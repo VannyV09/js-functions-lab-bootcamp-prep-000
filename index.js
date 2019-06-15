5369631e7f0717e14d1a36b76e1ef6b1c57d1382
vanessa lopez <vannyv09@yahoo.com>
2019-06-15 23:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function happyHolidayTo(IndependenceDay, you) {
   return 'Happy ${Independence Day}, ${you}!';
 }
 function holidayCountdown(MothersDay, days) {
-  return `It's ${20} days until ${Mothers Day}!`;
+  return `It's ${20} days until ${MothersDay}!`;
 }
