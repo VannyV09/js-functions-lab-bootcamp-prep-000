b026d94912498276ccd8699efbcae2f024c37d9f
vanessa lopez <vannyv09@yahoo.com>
2019-06-15 22:45:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function happyHolidays(string) {
   return ("Happy holidays!");
 }
 function say(happyHolidaysTo, you) {
-  return 'Happy holidays, ${you}!';
+  return '${Happy holidays}, ${you}!';
 }
