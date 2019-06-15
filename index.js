1ea6720fe0566857184f0bedbd0def14404b7620
vanessa lopez <vannyv09@yahoo.com>
2019-06-15 23:06:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function happyHolidaysTo(you) {
   return `Happy holidays, ${you}!`;
 }
 function happyHolidayTo(IndependenceDay, you) {
-  return 'Happy ${Independence Day}, ${you}!';
+  return `Happy ${Independence Day}, ${you}!`;
 }
